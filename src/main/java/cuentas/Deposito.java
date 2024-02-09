9be4db3e29ce4557646bcea3a6e6537cd5f49304
patriciabarrop <patriciabpadru@gmail.com>
2024-02-09 16:36:56 +0100

    Este es el commit de deposito

---
+++ src/main/java/cuentas/Deposito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cuentas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Deposito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
